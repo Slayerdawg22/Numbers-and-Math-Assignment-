6047485d29affe62bbaac041f97d86433802cb19
Raine DeVries <362571390@GoTVDSB.ca>
2024-09-09 09:13:40 -0400

    Add project files.

---
+++ Numbers and Math Assignment .1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Raine DeVries
+Console.WriteLine("My year of birth is " + (2024 - 15));
+Console.WriteLine();
+Console.WriteLine("What is the average");
+Console.WriteLine((14 + 15 + 16) / 3);
